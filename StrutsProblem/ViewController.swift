e85ca680f8221bb77927ed127ad54f743bc646e0
Paris Kapsouros <paris.kapsouros@gmail.com>
2015-12-16 00:52:45 +0200

    Initial Commit

---
+++ StrutsProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrutsProblem
+//
+//  Created by Paris Kapsouros on 16/12/15.
+//  Copyright © 2015 Paris Kapsouros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
