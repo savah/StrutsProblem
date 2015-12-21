039526ca63889c9f618779baf1fccc39680bb6d6
Paris Kapsouros <paris.kapsouros@gmail.com>
2015-12-22 01:23:32 +0200

    struts problem

--- StrutsProblem/ViewController.swift
+++ StrutsProblem/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeftView: UIView!
+    @IBOutlet weak var topRightView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
